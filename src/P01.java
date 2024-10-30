dd97a2f41dad903ca4e8e93709d358be3f59f301
vaishu1109 <vaishu.sg1109@gmail.com>
2024-10-30 21:09:55 +0530

    File Added problem solve

---
+++ src/P01.java
@@ -0,0 +1,19 @@
+public class P01 {
+    public static void main(String[] args) {
+        //Problem 1
+        //*
+        //* *
+        //* * *
+        //* * * *
+        //* * * * *
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
+}
