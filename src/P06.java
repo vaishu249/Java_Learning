67b06d4e233f04c4ecbd6e120dc4d84e5bbd73da
vaishu1109 <vaishu.sg1109@gmail.com>
2024-10-30 23:20:38 +0530

    File Added problem solve

---
+++ src/P06.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P06 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Program for Swap 2 no ...");
+        System.out.print("Enter No for a :");
+        int a=input.nextInt();
+        System.out.print("Enter No for b :");
+        int b=input.nextInt();
+        System.out.println("Number Before Swap a="+a+" b="+b);
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("Number After Swap a="+a+" b="+b);
+
+    }
+}
