131332a2f443471b8b74ef27fc2f181b83177700
vaishu1109 <vaishu.sg1109@gmail.com>
2024-10-31 13:33:15 +0530

    Arithmatic operation

---
+++ src/P07_Arithmatic_Operation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+// Program for Arithmatic operation
+public class P07_Arithmatic_Operation {
+    public static void main(String[] args) {
+        System.out.println("Program For Arithmatic Operation +,-,*,/,%");
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter 1st Number :");
+        int num1=input.nextInt();
+        System.out.print("Enter 2nd Number :");
+        int num2=input.nextInt();
+        System.out.println("Addition of numbers       : "+(num1+num2));
+        System.out.println("Substraction of numbers   : "+(num1-num2));
+        System.out.println("Multiplication of numbers : "+(num1*num2));
+        System.out.println("Division of numbers       : "+(num1/num2));
+        System.out.println("Remender after Division   : "+(num1%num2));
+
+        //output:
+        //Program For Arithmatic Operation +,-,*,/,%
+        //Enter 1st Number :20
+        //Enter 2nd Number :5
+        //Addition of numbers       : 25
+        //Substraction of numbers   : 15
+        //Multiplication of numbers : 100
+        //Division of numbers       : 4
+        //Remender after Division   : 0
+
+
+    }
+}
