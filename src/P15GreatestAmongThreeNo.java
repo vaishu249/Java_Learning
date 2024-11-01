46791df2e83b21eaa5304bed0ead1d6c64e5f501
vaishu1109 <vaishu.sg1109@gmail.com>
2024-11-01 14:23:51 +0530

    Greatest no among 3 no

---
+++ src/P15GreatestAmongThreeNo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class P15GreatestAmongThreeNo {
+    public static void main(String[] args) {
+        System.out.println("Program For finding largest no among 3 no ...");
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the Num1 : ");
+        int num1=input.nextInt();
+        System.out.print("Enter the Num2 : ");
+        int num2=input.nextInt();
+        System.out.print("Enter the Num1 : ");
+        int num3=input.nextInt();
+        if(num1>num2 && num1>num3){
+            System.out.println(num1+" : num1 is greater");
+        }
+        else if(num2>num3 && num2>num1){
+            System.out.println(num2+" : num2 is greater");
+        }
+        else{
+            System.out.println(num3+" : num3 is greater");
+        }
+    }
+}
