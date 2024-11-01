6e5b9054acef3783e9c7d770ef97dc2e5cbdf736
vaishu1109 <vaishu.sg1109@gmail.com>
2024-11-01 14:13:00 +0530

    Program for checking no positive or negative

---
+++ src/P13NumberPositiveOrNegative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P13NumberPositiveOrNegative {
+    public static void main(String[] args) {
+        System.out.println("Program For Identify No is positive,Negative or Equal to zero...");
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int num=input.nextInt();
+        if(num>0){
+            System.out.println(num+" is Positive");
+        }
+        else if(num<0){
+            System.out.println(num+" is Negative");
+        }
+        else{
+            System.out.println(num+" is equal to zero");
+        }
+    }
+}
