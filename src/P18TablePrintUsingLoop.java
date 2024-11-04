3a595f20529728f75a5f674f84cfb94327e3ed0f
vaishu1109 <vaishu.sg1109@gmail.com>
2024-11-04 21:20:31 +0530

    table printing using loop

---
+++ src/P18TablePrintUsingLoop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P18TablePrintUsingLoop {
+    public static void main(String[] args) {
+        System.out.println("Program For Print Table...\n");
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the no which you want to print table : ");
+        int num= input.nextInt();
+        int i=1;
+        while(i<=10){
+            System.out.println(num+" * "+i+" = "+(num*i));
+            i++;
+        }
+    }
+}
