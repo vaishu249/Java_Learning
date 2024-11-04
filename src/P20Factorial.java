6325e88cb495d723856a33eca217047738e8b29a
vaishu1109 <vaishu.sg1109@gmail.com>
2024-11-04 21:45:47 +0530

    print factorial

---
+++ src/P20Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P20Factorial {
+    public static void main(String[] args) {
+        System.out.println("Program For factorial of num..\n");
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the no where we have to print factorial number  : ");
+        int num =input.nextInt();
+        int sum=1;
+        int i=1;
+        while(i<=num){
+            sum*=i;
+            i++;
+        }
+        System.out.println("Factorial of "+num+" is "+sum);
+    }
+}
