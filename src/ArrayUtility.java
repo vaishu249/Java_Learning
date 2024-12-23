97fe947952b4769873170e17f153aa4114766030
vaishu1109 <vaishu.sg1109@gmail.com>
2024-12-23 22:44:09 +0530

    count the no in array occurance

--- src/ArrayUtility.java
+++ src/ArrayUtility.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
 public class ArrayUtility {
-    public static void inputFromUser(int[] arr) {
-        Scanner input =new Scanner(System.in);
-        int i=0;
-        while(i<arr.length){
-            System.out.print("Enter a element "+i+" ");
-            arr[i]=input.nextInt();
+    public static int[] inputFromUser() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        int i = 0;
+        while (i < arr.length) {
+            System.out.print("Enter a element " + i + " ");
+            arr[i] = input.nextInt();
             i++;
         }
+        return arr;
     }
 }
