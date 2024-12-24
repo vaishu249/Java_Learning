9d2258f3e3b3ffe2b861807ae337f0442a5400d1
vaishu1109 <vaishu.sg1109@gmail.com>
2024-12-24 14:26:57 +0530

    class obj practice

--- src/Learning/classAndObject.java
+++ src/Learning/classAndObject.java
@@ -1,4 +1,10 @@
 package Learning;
 
 public class classAndObject {
+    public int areaOfRectangle(int l,int w){
+        return l*w;
+    }
+    public float perimeterOfRectangle(int l, int w){
+        return (2*(l+w));
+    }
 }
---
+++ src/Learning/rectangle.java
@@ -0,0 +1,9 @@
+package Learning;
+
+public class rectangle {
+    public static void main(String[] args) {
+        classAndObject rec=new classAndObject();
+        System.out.println("Area of rectangle is : "+rec.areaOfRectangle(2,2));
+        System.out.println("Perimeter of rectangle is : "+rec.perimeterOfRectangle(2,2));
+    }
+}
