50ea09ecd6e74141a713f2a9d8b8cb082b01e2c4
vaishu1109 <vaishu.sg1109@gmail.com>
2024-12-28 14:57:35 +0530

    element delete in array

---
+++ src/P29DeleteOneEleAndAddInNew.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class P29DeleteOneEleAndAddInNew {
+    public static void main(String[] args) {
+        int[] arr=ArrayUtility.inputFromUser();
+        int[] newArr=new int[arr.length-1];
+
+    }
+
+    public static void addEleInArray(int[] arr,int[] newArr) {
+        Scanner input= new Scanner(System.in) ;
+        int ele= input.nextInt();
+        int i=0;
+        while(i<arr.length){
+
+        }
+    }
+}
