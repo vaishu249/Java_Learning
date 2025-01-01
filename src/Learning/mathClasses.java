f47b7895aaff5c991e80ab5eb93d3e40d3acf75f
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-01 19:19:29 +0530

    math function

---
+++ src/Learning/mathClasses.java
@@ -0,0 +1,12 @@
+package Learning;
+
+public class mathClasses {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-20));
+        System.out.println(Math.max(1,2));
+        System.out.println(Math.min(12,2));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.round(13.6));
+    }
+}
