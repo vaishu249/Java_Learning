e03d9c354c101a5e83827547aec833d6ca2f1a9c
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-02 12:34:34 +0530

    inheritance program

---
+++ src/Learning/StringBufferUse.java
@@ -0,0 +1,10 @@
+package Learning;
+
+public class StringBufferUse {
+    public static void main(String[] args) {
+        StringBuffer sb= new StringBuffer("Hello");
+        sb.append("vaishnavi");
+        sb.append(55);
+        System.out.println(sb);
+    }
+}
