a8ceb2be6d56d9e0309c12e8199de82e7e166ddf
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-11 19:31:55 +0530

    palindrome array or not

---
+++ src/P33MergeTwoSortedArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class P33MergeTwoSortedArray {
+    public int[] sorted(int[] arr){
+        for (int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public int[] inputFromUser() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        int i = 0;
+        while (i < arr.length) {
+            System.out.print("Enter a element " + (i+1) + " ");
+            arr[i] = input.nextInt();
+            i++;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        P33MergeTwoSortedArray obj=new P33MergeTwoSortedArray();
+        int[] arr=obj.inputFromUser();
+        int[] arr2=obj.inputFromUser();
+        int[] result=new int[arr.length+arr2.length];
+        System.out.println(obj.sorted(arr2));
+
+    }
+
+}
