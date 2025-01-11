dcc34bacef1f0dbf984e2e35f9871037b293caf8
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-11 22:51:40 +0530

    reverse array

---
+++ src/P35ReverseArray.java
@@ -0,0 +1,26 @@
+public class P35ReverseArray {
+    public static void main(String[] args) {
+        int[] arr=ArrayUtility.inputFromUser();
+        System.out.println("Your original array is.. ");
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        reverse(arr);
+        System.out.println("\nYour reverse array is.. ");
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+    public static int[] reverse(int[] arr) {
+        int i=0;
+        while(i<arr.length/2){
+            int temp=arr[i];
+            arr[i]=arr[arr.length-1-i];
+            arr[arr.length-1-i]=temp;
+            i++;
+        }
+        return arr;
+    }
+}
