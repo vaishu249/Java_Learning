f003689bada8a8c945044a7172045d83b9428b62
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-16 11:27:56 +0530

    greater no  finding between 3 no

---
+++ src/P37MinimumOfTwoNo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P37MinimumOfTwoNo {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter 3 element : ");
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=input.nextInt();
+        int greater=(a>b)?(a>c?a:c):(b>c?b:c);
+        System.out.println("greater no : "+greater);
+
+    }
+}
