643eb9e15da93ab13b1dc219ceb73492d0b1db66
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-18 22:03:34 +0530

    * * * * * * * * * * * * * * *

--- src/PatternSolving/Pattern_03.java
+++ src/PatternSolving/Pattern_03.java
@@ -1,13 +1,24 @@
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
 package PatternSolving;
 
 import java.util.Scanner;
 
-public class CommonInput {
-    public static void takeInput(){
+public class Pattern_03 {
+    public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter No of Rows : ");
         int row= input.nextInt();
         System.out.println("Enter No of Columns : ");
         int col= input.nextInt();
+        for(int i=row;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
