1c0b6ea385a66f325a6e2c13c6e82b50cc41c826
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-23 19:49:33 +0530

    map

---
+++ src/Collection/MapCollectionFramework.java
@@ -0,0 +1,22 @@
+package Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapCollectionFramework {
+    public static void main(String[] args) {
+        Map<Integer,String> m=new HashMap<>();
+        m.put(1,"Vaishnavi");
+        m.put(2,"Sneha");
+        m.put(3,"abc");
+        m.put(3,"Vaishnavi");
+        m.put(5,"Sneha");
+        m.put(6,"abc");
+        for(int n:m.keySet()){
+            System.out.printf("%d : %s \n",n,m.get(n));
+        }
+        System.out.println(m.remove(5));
+        System.out.println(m.size());
+
+    }
+}
