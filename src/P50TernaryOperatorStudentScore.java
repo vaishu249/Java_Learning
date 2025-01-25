732cd64451a88e8aefe16fc30c2ecb362c1bb7aa
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-25 20:29:41 +0530

    half

---
+++ src/P50TernaryOperatorStudentScore.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class P50TernaryOperatorStudentScore {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the ");
+    }
+}
