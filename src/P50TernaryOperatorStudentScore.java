f34b87832ea6b559d60d316d7c6db911dc6c8416
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-26 19:16:53 +0530

    Ternary operator use score card

--- src/P50TernaryOperatorStudentScore.java
+++ src/P50TernaryOperatorStudentScore.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class P50TernaryOperatorStudentScore {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.println("Enter the ");
+        System.out.println("Enter your Score : ");
+        float score= input.nextFloat();
+        String scoreCategory= (score>=80) ? "High" : (score>=60 ? "Moderate" : "Low");
+        System.out.println("Your Score is \""+scoreCategory+"\"");
     }
 }
