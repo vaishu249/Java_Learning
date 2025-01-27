92b7cb60ce8994d4c88329a36f0478881f6fb682
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-27 18:15:50 +0530

    half pattern logic

---
+++ src/PatternSolving/Pattern_14.java
@@ -0,0 +1,24 @@
+package PatternSolving;
+
+import java.util.Scanner;
+
+public class Pattern_14 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter No of Rows : ");
+        int row= input.nextInt();
+        System.out.println("Enter No of Columns : ");
+        int col= input.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=col;j++){
+                if(i==j || i+j==row+1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
