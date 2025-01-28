2e2a951f491291b00eed627b46a01ccadda9879f
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-28 20:49:08 +0530

    Thread join

--- src/Learning/Multithreading/ThreadPriority/TestRunnableThreadPriority.java
+++ src/Learning/Multithreading/ThreadPriority/TestRunnableThreadPriority.java
@@ -3,13 +3,14 @@ package Learning.Multithreading.ThreadPriority;
 import Learning.Multithreading.ImplementingRunnableInterface.PrintTaskThreadPriority;
 
 public class TestRunnableThreadPriority {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         PrintTaskThreadPriority p1=new PrintTaskThreadPriority('*');
         PrintTaskThreadPriority p2=new PrintTaskThreadPriority('#');
         PrintTaskThreadPriority p3=new PrintTaskThreadPriority('$');
         PrintTaskThreadPriority p4=new PrintTaskThreadPriority('&');
         Thread t1=new Thread(p1);
         t1.start();
+        t1.join();
         t1.setPriority(Thread.MIN_PRIORITY);
         Thread t2=new Thread(p2);
         t2.start();
