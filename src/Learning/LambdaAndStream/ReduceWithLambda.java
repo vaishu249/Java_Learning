0db46a0b5dd809ac03b3dae0bbcf96765ba69173
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-31 21:26:25 +0530

    reduce with lambda and using filter as well

--- src/Learning/LambdaAndStream/ReduceWithLambda.java
+++ src/Learning/LambdaAndStream/ReduceWithLambda.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class ReduceWithLambda {
     public static void main(String[] args) {
         List<Integer> no= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        System.out.println(no.stream().reduce(0,(a,b)->a+b));
+        System.out.println("Addition of list is of all element : "+no.stream().reduce(0,(a,b)->a+b));
+        System.out.println("Addition of Even no is : "+no.stream().filter(l->l%2==0).reduce(0,(a,b)->a+b));
+
     }
 }
