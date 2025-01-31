d2c8107ad7e353a4f81ef813636c8f0cdefea049
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-31 23:44:00 +0530

    String concatination

---
+++ src/ProblemSolving/P52StringConcatInLambda.java
@@ -0,0 +1,11 @@
+package ProblemSolving;
+
+import java.util.List;
+
+public class P52StringConcatInLambda {
+    public static void main(String[] args) {
+        List<String> concat=List.of("I","Am","Good","Girl","AS","The");
+        String result=concat.stream().filter(s->s.length()>2).reduce("",(a,b)-> a + " " + b);
+        System.out.println(result);
+    }
+}
