e501319295b9c65943ba3e39fcd8ccfb7c5e6542
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-01 13:06:18 +0530

    Functional Programing way of factorial

--- src/ProblemSolving/P54FactorialWith2Ways/FactorialTwoWay.java
+++ src/ProblemSolving/P54FactorialWith2Ways/FactorialTwoWay.java
@@ -1,11 +1,16 @@
 package ProblemSolving.P54FactorialWith2Ways;
 
+import java.util.stream.IntStream;
+
 public class FactorialTwoWay {
     public static void main(String[] args) {
         int fact=factorial(5);
+        System.out.println("Normal Programming..");
         System.out.println(fact);
 
-
+        System.out.println("Functional programing...");
+        int num=5;
+        IntStream.rangeClosed(2,num).reduce((a,b)->a*b).ifPresent(System.out::println);
     }
     public static int factorial(int num){
         if(num==0||num==1){
