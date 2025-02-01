38073d8655f07bc75ca25fc0ce27e0d1c16fab68
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-01 14:02:20 +0530

    Old list : [10, 1, 1, 10, 2, 4, 5, 4, 5, 6, 78, 8] New List : [10, 1, 2, 4, 5, 6, 78, 8]

---
+++ src/ProblemSolving/P55OptionalClass/P56DistinctInArrayStream.java
@@ -0,0 +1,13 @@
+package ProblemSolving.P55OptionalClass;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class P56DistinctInArrayStream {
+    public static void main(String[] args) {
+        List<Integer> marks=List.of(10,1,1,10,2,4,5,4,5,6,78,8);
+        List<Integer> newMarks=marks.stream().distinct().collect(Collectors.toList());
+        System.out.println("Old list : "+marks);
+        System.out.println("New List : "+newMarks);
+    }
+}
