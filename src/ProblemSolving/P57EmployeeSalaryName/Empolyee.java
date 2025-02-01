fa36007a00883f8f521d72d3f857a6a42d6ef89b
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-01 14:18:53 +0530

    employee detail constructor and get seeter

---
+++ src/ProblemSolving/P57EmployeeSalaryName/Empolyee.java
@@ -0,0 +1,27 @@
+package ProblemSolving.P57EmployeeSalaryName;
+
+public class Empolyee {
+    private String name;
+    private int salary;
+
+    public Empolyee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
