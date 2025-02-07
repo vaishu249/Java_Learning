bf1f5b0627728b06a69bcaee51580ac8bd74cf8f
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-07 19:45:38 +0530

    half code of palindrome string

---
+++ src/ProblemSolving/P68StringPalindromeOrNotUsingRecursion.java
@@ -0,0 +1,15 @@
+package ProblemSolving;
+
+public class P68StringPalindromeOrNotUsingRecursion {
+    public static void main(String[] args) {
+        //not clear logic
+    }
+    public static boolean palindrome(String str ,String str2){
+        if(str.equalsIgnoreCase(str2)){
+            return true;
+        }
+        else{
+        }
+        return false;
+    }
+}
