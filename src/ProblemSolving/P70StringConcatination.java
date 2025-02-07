b56c0ea5da3fb41795713f36a4d02748f49f5a25
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-07 19:56:31 +0530

    String Concatination

---
+++ src/ProblemSolving/P70StringConcatination.java
@@ -0,0 +1,9 @@
+package ProblemSolving;
+
+public class P70StringConcatination {
+    public static void main(String[] args) {
+        String str="vaishnavi";
+        String str2=" gundgire";
+        System.out.println(str.concat(str2).toUpperCase());
+    }
+}
