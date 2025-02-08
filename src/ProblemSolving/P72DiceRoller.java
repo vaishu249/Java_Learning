aa9f66903f56a8d30f9b2c2fd9e496364808d9af
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-08 23:19:38 +0530

    math.random

---
+++ src/ProblemSolving/P72DiceRoller.java
@@ -0,0 +1,8 @@
+package ProblemSolving;
+
+public class P72DiceRoller {
+    public static void main(String[] args) {
+        int a=(int)(Math.random()*6)+1;
+        System.out.println(a);
+    }
+}
