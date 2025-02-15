ca91a63a1f8dcab209ef8fb7fa5a2f387229a5ff
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-15 22:17:08 +0530

    no guessing

---
+++ src/ProblemSolving/P73NoGuessingUsingMath.java
@@ -0,0 +1,13 @@
+package ProblemSolving;
+
+import java.util.Scanner;
+
+public class P73NoGuessingUsingMath {
+    public static void main(String[] args) {
+        int a=(int)(Math.random()*0)+6;
+        System.out.println(a);
+        Scanner input=new Scanner(System.in);
+        int x=input.nextInt();
+
+    }
+}
