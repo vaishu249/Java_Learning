47e2830636cfb27faff02693cddf4fd8e84f3ece
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-16 23:26:17 +0530

    final keyword

--- src/ProblemSolving/P74FinalKeywordUse.java
+++ src/ProblemSolving/P74FinalKeywordUse.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class P74FinalKeywordUse {
     public static void main(String[] args) {
         final  float PI=3.14f;
+        // can not assign PI=34;
         Scanner input=new Scanner(System.in);
         int r=input.nextInt();
         System.out.println("Area Of circle : "+area(r,PI));
