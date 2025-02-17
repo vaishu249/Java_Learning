f5712a62afe1213c0fab0620be1dda4f41fbc84e
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-17 09:33:07 +0530

    No guessing game

--- src/ProblemSolving/P73NoGuessingUsingMath.java
+++ src/ProblemSolving/P73NoGuessingUsingMath.java
@@ -7,7 +7,20 @@ public class P73NoGuessingUsingMath {
         int a=(int)(Math.random()*0)+6;
         System.out.println(a);
         Scanner input=new Scanner(System.in);
-        int x=input.nextInt();
+        int x;
+        do{
+            System.out.println("Enter the guess no : ");
+            x= input.nextInt();
+            if(x>a){
+                System.out.println("Enter Shorter no...");
+            } else if (x<a) {
+                System.out.println("Enter Greater no...");
+            } else if (x<0) {
+                System.out.println("enter positive no ...");
+            } else{
+                System.out.println("Great you Enter correct no...");
+            }
+        }while(a!=x);
 
     }
 }
